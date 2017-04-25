3b0f06334c1b60e023f80f50f940062087a17d86
Tom Heeney <tom.heeney@gmail.com>
2017-04-25 15:53:58 +0100

    Added test cases for challenges 10-12

--- Set2/test.py
+++ Set2/test.py
@@ -1,4 +1,7 @@
 import Challenge09
+import Challenge10
+import Challenge11
+import Challenge12
 import unittest
 
 
@@ -9,6 +12,19 @@ class testSet2(unittest.TestCase):
     def testChallenge09(self):
         expected = b'YELLOW SUBMARINE\x04\x04\x04\x04'
         self.assertEqual(Challenge09.main(), expected)
+
+    def testChallenge10(self):
+        with open('10-out.txt', 'rb') as f:
+            expected = f.read()
+        self.assertEqual(Challenge10.main(), expected)
+
+    def testChallenge11(self):
+        self.assertIs(Challenge11.main()[0], True)
+
+    def testChallenge12(self):
+        with open('12-out.txt', 'rb') as f:
+            expected = f.read()
+        self.assertEqual(Challenge12.main(), expected)
 
 
 if __name__ == '__main__':
